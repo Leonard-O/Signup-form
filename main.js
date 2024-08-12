a86c0fdca3676d0f6b58e17e37a95335bdee3413
zannoh <realcodedevs@gmail.com>
2024-08-12 16:10:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const signUp = document.querySelector('#signUp');
+const signIn = document.querySelector('#signIn');
+const logIn = document.querySelector('#logIn');
+const btn = document.querySelector('#btn');
+const create = document.querySelector('#create');
+
+const btnToggle = () => {
+	signUp.style.display = 'none';
+	signIn.style.display = 'flex';
+};
+
+logIn.addEventListener('click', () => {
+	signUp.style.display = 'none';
+	signIn.style.display = 'flex';
+});
+create.addEventListener('click', () => {
+	signUp.style.display = 'flex';
+	signIn.style.display = 'none';
+});
